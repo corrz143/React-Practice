f7682ca2f320214c4b66e1513f62ceb3841318ff
Corazon Halstead <corrzhalstead143@gmail.com>
2021-09-10 13:09:18 -0700

    React practice Globomantics

---
+++ my-app/src/main-page/Homepage.js
@@ -0,0 +1,64 @@
+import { useEffect, useState, useMemo} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './homepage.css';
+import Header from "./Header";
+import FeaturedHouse from "./featured-house";
+import SearchResults from "../search-results/search";
+import HouseFromQuery from "../house/HouseFromQuery";
+import HouseFilter from "./house-filter";
+
+
+const App =() => {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+
+  }, [allHouses])
+
+  
+
+  return (
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+        <HouseFilter allHouses={allHouses} />
+
+
+        <Switch>
+          <Route path="/searchresults/:country">
+            <SearchResults allHouses={allHouses} />
+          </Route>
+          
+          <Route path="/house/:id">
+            <HouseFromQuery allHouses={allHouses} />
+          </Route>
+
+
+          <Route path="/">    
+            <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+          </Route>
+        </Switch>
+
+      </div>
+
+    
+    </Router>
+    
+  );
+}
+
+export default App;
+
