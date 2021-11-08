0701d867105d99948a2e9028752b1867d1218434
Corazon Halstead <corrzhalstead143@gmail.com>
2021-11-07 21:44:13 -0800

    React Components

---
+++ redux-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter }from 'react-router-dom';
+import { render} from 'react-dom';
+// import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+// import store from './redux/store';
+
+
+render(
+  // <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  // </Provider>,
+  document.getElementById('root')
+);
+
---
+++ redux-app/src/redux/root-reducer.js
@@ -0,0 +1,10 @@
+//Parent Reducer
+
+import { combineReducers } from "redux";
+
+import userReducer from "./user/user.reducer";
+
+
+export default combineReducers({
+    user: userReducer
+});
